520bae11ba0867241c01942926a3e938bc3dcdf8
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-06 15:13:55 +0300

    Error correction3

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -23,25 +23,25 @@ const Message = (props: MessagePropsType) => {
         <div id={'hw1-message-' + props.message.id} className={s.message}>
             <div className={s.imageAndText}>
                 <img
-                    id={'hw1-avatar-' + props.message.id}
+                    id={'hw1-avatar-1' + props.message.id}
                     // создаёт студент
                     src ={props.message.user.avatar}
                     //
                 />
                 <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.id} className={s.name}>
+                    <div id={'hw1-name-1' + props.message.id} className={s.name}>
                         {/*создаёт студент*/}
                         {props.message.user.name}
                         {/**/}
                     </div>
-                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
+                    <pre id={'hw1-text-1' + props.message.id} className={s.messageText}>
                         {/*создаёт студент*/}
                         {props.message.message.text}
                         {/**/}
                     </pre>
                 </div>
             </div>
-            <div id={'hw1-time-' + props.message.id} className={s.time}>
+            <div id={'hw1-time-1' + props.message.id} className={s.time}>
                 {/*создаёт студент*/}
                 {props.message.message.time}
                 {/**/}
