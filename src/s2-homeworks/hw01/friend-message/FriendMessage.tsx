5da8e31d695d8e771a839ddf44e517d048646ac8
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-06 19:45:36 +0300

    Correction of errors props and styles

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,25 +1,10 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
-
-
-export type FriendMessagePropsType = {
-
-    message:{
-        id:number
-        user:{
-            avatar:string
-            name:string
-        }
-        message:{
-            text:string
-            time:string
-        }
-    }
-}
+import { MessageType } from '../HW1'
 
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: FriendMessagePropsType) => {
+const FriendMessage = (props: {message: MessageType}) => {
     return (
         <div
             id={'hw1-friend-message-' + props.message.id}
