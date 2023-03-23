e05b082c6e06f2783d7a03f51e1e4a5f5edad07e
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-03-23 23:14:13 +0300

    Application theme change implementation and stylization

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
-const initState = {
+const SET_THEME_ID = 'SET_THEME_ID'
+
+export const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+
+export const themeReducer = (state = initState, action: ActionType): typeof initState => { // fix any
     switch (action.type) {
-        // дописать
+        case SET_THEME_ID:{
+            return {...state,themeId:action.id}
+        }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ActionType = changeThemeIdType
+
+type changeThemeIdType = ReturnType<typeof changeThemeId>
+
+export const changeThemeId = (id:number) => {
+    return{
+        type: SET_THEME_ID,
+        id
+    }as const
+}
